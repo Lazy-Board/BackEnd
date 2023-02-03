14196c4bb4ec3068a8832b74d54cdef403e3c514
codesche <codesche@gmail.com>
2023-02-03 17:31:18 +0900

    Project Setting

---
+++ src/main/java/com/example/lazier/LazierApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lazier/LazierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
