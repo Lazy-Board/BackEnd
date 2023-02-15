feb6fbf07b9c61c4140215716958c1717e3cd205
karenyoon <yoonyoonsung4@gmail.com>
2023-02-16 03:44:22 +0900

    Weather_Feat: #39 - controller에 swagger 적용

--- src/main/java/com/example/lazier/WeatherModule/controller/WeatherController.java
+++ src/main/java/com/example/lazier/WeatherModule/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.example.lazier.WeatherModule.controller;
 
 import com.example.lazier.WeatherModule.service.WeatherService;
+import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController {
 
     private final WeatherService weatherService;
-
+    @ApiOperation(value = "사용자가 지정한 지역의 날씨 정보를 조회 할 수 있는 api 입니다.")
     @GetMapping
     public ResponseEntity<?> getWeather(HttpServletRequest request) {
         return new ResponseEntity<>(weatherService.getWeather(request), HttpStatus.OK);
