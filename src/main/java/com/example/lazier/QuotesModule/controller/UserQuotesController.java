92d39791bee43f1d668ce1e96abbb7296542f950
karenyoon <yoonyoonsung4@gmail.com>
2023-02-16 23:24:48 +0900

    Quotes_Refactor: #45 - controller annotation 수정

--- src/main/java/com/example/lazier/QuotesModule/controller/UserQuotesController.java
+++ src/main/java/com/example/lazier/QuotesModule/controller/UserQuotesController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,7 @@ public class UserQuotesController {
         return new ResponseEntity<>(userQuoteService.get(userId), HttpStatus.OK);
     }
 
-    @PostMapping("/update")
+    @PutMapping
     public ResponseEntity<?> update(HttpServletRequest request,
         @RequestBody @Valid UserQuotesInput parameter) {
         String userId = (String) request.getAttribute("userId");
