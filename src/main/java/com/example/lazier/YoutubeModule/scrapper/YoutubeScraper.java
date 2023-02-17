178c09e6db02b928a2df6ab5d5e0ac868c28a3b7
IndeokDukeSeo <dukeindream@gmail.com>
2023-02-17 10:39:46 +0900

    Youtube_Feat: #7 Youtube 기능 완성(Refactoring 필요) - 오래된 동영상정보 삭제 스케쥴링 추가    * YoutubeServiceImpl - 각 기능에 대한 스크래치 테스트 완료    * scratch_youtube.http - mariaDB 오류로 h2-DB 대체연동 상태. - CustomException 적용을 위한 클래스 및 핸들러 생성    * CustomException.java    * 적용점 미확인 상태

--- src/main/java/com/example/lazier/YoutubeModule/scrapper/YoutubeScraper.java
+++ src/main/java/com/example/lazier/YoutubeModule/scrapper/YoutubeScraper.java
@@ -22,92 +22,108 @@ import java.util.List;
 
 @Component
 public class YoutubeScraper {
-    ScrapedResult scrapResult = new ScrapedResult();
-    String html = null;
-    HttpURLConnection con;
-    String YOUTUBE_URL = "https://www.youtube.com/feed/trending";
 
-    public ScrapedResult crawl() {
-        scrapResult = new ScrapedResult();
-        try {
-            URL url = new URL(YOUTUBE_URL);
-            con = (HttpURLConnection) url.openConnection();
-            con.setUseCaches(true);
-            con.setConnectTimeout(5000);
-            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            while (true) {
-                String line = br.readLine();
-                if (line == null) {
-                    break;
-                }
-                sb.append(line + "\n");
-                html = sb.toString();
-                br.close();
-                con.disconnect();
-                String str = null;
-                String str2 = null;
-                Document document = Jsoup.parse(html);
-                //<script> </script> 태그 찾기
-                Elements elements = document.getElementsByTag("script");
-                for (
-                        Element element : elements) {
-                    //text 중 "var ytInitialData" 로 시작하는 html 찾기
-                    if (element.html().startsWith("var ytInitialData")) {
-                        str = element.html();
-                        str = str.replace("var ytInitialData = ", "");
-                        str2 = str.substring(0, str.lastIndexOf(";"));
-                    }
-                }
-                //찾아낸 html 에 대하여 Json으로 파싱 후 관련된 부분까지 drill
+  ScrapedResult scrapResult = new ScrapedResult();
+  String html = null;
+  HttpURLConnection con;
+  String YOUTUBE_URL = "https://www.youtube.com/feed/trending";
 
-                Gson gson = new Gson();
-                JsonObject jsonObject = gson.fromJson(str2, JsonObject.class);
+  public ScrapedResult crawl() {
+    scrapResult = new ScrapedResult();
+    try {
+      URL url = new URL(YOUTUBE_URL);
+      con = (HttpURLConnection) url.openConnection();
+      con.setUseCaches(true);
+      con.setConnectTimeout(5000);
+      BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      StringBuilder sb = new StringBuilder();
+      while (true) {
+        String line = br.readLine();
+        if (line == null) {
+          break;
+        }
+        sb.append(line + "\n");
+      }
+      html = sb.toString();
+      br.close();
+      con.disconnect();
+      String str;
+      String str2 = null;
+      Document document = Jsoup.parse(html);
+      //<script> </script> 태그 찾기
+      Elements elements = document.getElementsByTag("script");
+      for (
+          Element element : elements) {
+        //text 중 "var ytInitialData" 로 시작하는 html 찾기
+        if (element.html().startsWith("var ytInitialData")) {
+          str = element.html();
+          str = str.replace("var ytInitialData = ", "");
+          str2 = str.substring(0, str.lastIndexOf(";"));
+        }
+      }
+      //찾아낸 html 에 대하여 Json으로 파싱 후 관련된 부분까지 drill
 
-                // 비디오클립 리스트를 JsonArray 으로 받아오기
-                JsonArray hotClipArray = jsonObject.getAsJsonObject("contents")
-                        .getAsJsonObject("twoColumnBrowseResultsRenderer")
-                        .get("tabs").getAsJsonArray().get(0)
-                        .getAsJsonObject()
-                        .getAsJsonObject("tabRenderer")
-                        .getAsJsonObject("content")
-                        .getAsJsonObject("sectionListRenderer")
-                        .getAsJsonArray("contents").get(3).getAsJsonObject()
-                        .getAsJsonObject("itemSectionRenderer")
-                        .getAsJsonArray("contents").get(0).getAsJsonObject()
-                        .getAsJsonObject("shelfRenderer")
-                        .getAsJsonObject("content")
-                        .getAsJsonObject("expandedShelfContentsRenderer")
-                        .getAsJsonArray("items");
+      Gson gson = new Gson();
+      JsonObject jsonObject = gson.fromJson(str2, JsonObject.class);
 
-                //Array 순서대로 돌며 youtube 객체로 저장
-                List<Youtube> youtubeList = new ArrayList<>();
+      // 비디오클립 리스트를 JsonArray 으로 받아오기
+      JsonArray hotClipArray = jsonObject.getAsJsonObject("contents")
+          .getAsJsonObject("twoColumnBrowseResultsRenderer")
+          .get("tabs").getAsJsonArray().get(0)
+          .getAsJsonObject()
+          .getAsJsonObject("tabRenderer")
+          .getAsJsonObject("content")
+          .getAsJsonObject("sectionListRenderer")
+          .getAsJsonArray("contents").get(3).getAsJsonObject()
+          .getAsJsonObject("itemSectionRenderer")
+          .getAsJsonArray("contents").get(0).getAsJsonObject()
+          .getAsJsonObject("shelfRenderer")
+          .getAsJsonObject("content")
+          .getAsJsonObject("expandedShelfContentsRenderer")
+          .getAsJsonArray("items");
 
-                for (int i = 0; i < hotClipArray.size(); i++) {
-                    Youtube youtube = new Youtube();
-                    JsonObject baseObject = hotClipArray.get(i).getAsJsonObject()
-                            .getAsJsonObject("videoRenderer");
-                    youtube.setVideoId(baseObject.get("videoId").getAsString());
-                    youtube.setContentName(baseObject.getAsJsonObject("title").getAsJsonArray("runs").get(0).getAsJsonObject().get("text").getAsString());
-                    youtube.setChannelName(baseObject.getAsJsonObject("longBylineText").getAsJsonArray("runs").get(0).getAsJsonObject().get("text").getAsString());
-                    youtube.setImagePath(baseObject.getAsJsonObject("thumbnail")
-                            .getAsJsonArray("thumbnails").get(0).getAsJsonObject().get("url").getAsString());
-                    youtube.setCreatedAt(baseObject.getAsJsonObject("publishedTimeText").get("simpleText").getAsString());
-                    youtube.setLength(baseObject.getAsJsonObject("lengthText").get("simpleText").getAsString());
-                    youtube.setNumViewers(baseObject.getAsJsonObject("viewCountText")
-                            .get("simpleText").getAsString());
-                    youtube.setUpdatedAt(LocalDateTime.now());
+      //Array 순서대로 돌며 youtube 객체로 저장
+      List<Youtube> youtubeList = new ArrayList<>();
+
+      for (int i = 0; i < hotClipArray.size(); i++) {
+        Youtube youtube = new Youtube();
+        JsonObject baseObject = hotClipArray.get(i).getAsJsonObject()
+            .getAsJsonObject("videoRenderer");
+        youtube.setVideoId(baseObject.get("videoId").getAsString());
+        youtube.setContentName(
+            baseObject.getAsJsonObject("title").getAsJsonArray("runs").get(0).getAsJsonObject()
+                .get("text").getAsString());
+        youtube.setChannelName(
+            baseObject.getAsJsonObject("longBylineText").getAsJsonArray("runs").get(0)
+                .getAsJsonObject().get("text").getAsString());
+        youtube.setImagePath(baseObject.getAsJsonObject("thumbnail")
+            .getAsJsonArray("thumbnails").get(0).getAsJsonObject().get("url").getAsString());
+
+        //수정필요
+        String dateString = baseObject.getAsJsonObject("publishedTimeText").get("simpleText")
+            .getAsString();               //*일 전   //*주 전
+        String beforeText = dateString.substring(0, dateString.indexOf(" ")).trim();
+        String beforeDayOrWeek = beforeText.substring(beforeText.length() - 1);
+        int beforeNum = Integer.parseInt(beforeText.substring(0, beforeText.length() - 1));
+        if (beforeDayOrWeek.equals("일"))youtube.setCreatedAt(LocalDateTime.now().minusDays(beforeNum));
+        else youtube.setCreatedAt(LocalDateTime.now().minusWeeks(beforeNum));
+
+        youtube.setLength(
+            baseObject.getAsJsonObject("lengthText").get("simpleText").getAsString());
+        youtube.setHit(baseObject.getAsJsonObject("viewCountText")
+            .get("simpleText").getAsString());
+        youtube.setUpdatedAt(LocalDateTime.now());
 
 //                컨텐츠 설명은 우선 저장하지 않는 것으로
 //                youtube.setContentExplanation(baseObject.getAsJsonObject("descriptionSnippet")
 //                        .getAsJsonArray("runs").get(0).getAsJsonObject().get("text").getAsString());
-                    youtubeList.add(youtube);
-                }
-                scrapResult.setYoutubeList(youtubeList);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return scrapResult;
+        youtubeList.add(youtube);
+      }
+      scrapResult.setYoutubeList(youtubeList);
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+    return scrapResult;
+  }
 }
--- src/main/java/com/example/lazier/YoutubeModule/service/YoutubeService.java
+++ src/main/java/com/example/lazier/YoutubeModule/service/YoutubeService.java
@@ -1,14 +1,10 @@
 package com.example.lazier.YoutubeModule.service;
 
-import com.example.lazier.YoutubeModule.persist.repository.YoutubeRepository;
-import com.example.lazier.YoutubeModule.scrapper.YoutubeScraper;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.example.lazier.YoutubeModule.model.Youtube;
+import java.util.List;
 
-@Service
-@AllArgsConstructor
-public class YoutubeService {
-    private final YoutubeRepository youtubeRepository;
-    private final YoutubeScraper youtubeScraper;
+public interface YoutubeService {
+
+  List<Youtube> getYoutube();
 
 }
