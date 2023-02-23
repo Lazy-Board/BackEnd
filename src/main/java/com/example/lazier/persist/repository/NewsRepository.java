2e2409c92529494a0758ecac5262dffce0cd6d37
IndeokDukeSeo <dukeindream@gmail.com>
2023-02-23 19:46:14 +0900

    News_Feat: #85 - News Crawling_service&controller 완성 1) NewsDb update Scheduling 구현 : News / News Press 에 대한 db 받아오기 스케줄링 구현 : 오래된 db에 대한 db삭제 구현

--- src/main/java/com/example/lazier/persist/repository/NewsRepository.java
+++ src/main/java/com/example/lazier/persist/repository/NewsRepository.java
@@ -1,6 +1,7 @@
 package com.example.lazier.persist.repository;
 
 import com.example.lazier.persist.entity.module.News;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface NewsRepository extends JpaRepository<News, String> {
 
   List<News> findTop10ByPressIdOrderByUpdatedAtDesc(String pressId);
+  List<News> findAllByUpdatedAtBefore(LocalDateTime localDateTime);
+
 
 
 }
