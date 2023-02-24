c1599cd68d6af600a84751256f683be8e72b989c
IndeokDukeSeo <dukeindream@gmail.com>
2023-02-24 20:57:06 +0900

    News&Youtube_Fix: Swagger-Ui 적용 1) SwaggerConfig: 해당 ui적용을 위해 기존 Config 수정 2) SecurityConfig: swagger-ui 표시를위해 security 설정 일부 수정.

--- src/main/java/com/example/lazier/controller/NewsController.java
+++ src/main/java/com/example/lazier/controller/NewsController.java
@@ -2,6 +2,7 @@ package com.example.lazier.controller;
 
 import com.example.lazier.dto.module.NewsDto;
 import com.example.lazier.service.module.NewsService;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,13 @@ public class NewsController {
    * 동작 확인용 // 미사용 컨트롤러,
    * TODO 배포시 주석처리 필요
    */
+  @ApiOperation(value = "미사용 API입니다:News DB정보 초기화용// 추후 삭처리 예정 ")
   @GetMapping("/init")
   public ResponseEntity<?> init() {
     this.newsService.dbInit();
     return ResponseEntity.ok("news DB가 크롤링되었습니다.");
   }
-
+  @ApiOperation(value = "사용자가 선택한 언론사별 뉴스 리스트 JSON 반환. ")
   @GetMapping
   public ResponseEntity<?> showNews(HttpServletRequest request) {
     List<NewsDto> newsDtoList = this.newsService.showNewsByUser(request);
