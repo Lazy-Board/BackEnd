897ca27dcb94995bf8856200ef1599e87f6be120
oooo91 <74234719+oooo91@users.noreply.github.com>
2023-02-26 00:24:39 +0900

    Update CustomAuthenticationEntryPoint.java

--- src/main/java/com/example/lazier/exception/user/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/lazier/exception/user/CustomAuthenticationEntryPoint.java
@@ -27,6 +27,8 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException e) throws IOException {
 		log.info("CustomAuthenticationEntryPoint");
+		log.warn("jwt-exception: " + request.getAttribute("exception"));
+		log.warn("nonjwt-exception: " + request.getAttribute("nonjwtexception"));
 
 		Object errorObject = request.getAttribute("exception");
 		if (errorObject != null) {
