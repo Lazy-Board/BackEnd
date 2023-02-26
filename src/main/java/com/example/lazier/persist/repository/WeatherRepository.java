d1068cf570e5ed5564fd552bcd13070844372fc7
karenyoon <yoonyoonsung4@gmail.com>
2023-02-27 03:47:32 +0900

    Weather_refactor : weather 2일전 데이터 삭제 로직 추가

--- src/main/java/com/example/lazier/persist/repository/WeatherRepository.java
+++ src/main/java/com/example/lazier/persist/repository/WeatherRepository.java
@@ -3,10 +3,12 @@ package com.example.lazier.persist.repository;
 
 import com.example.lazier.persist.entity.module.Weather;
 import com.example.lazier.persist.entity.user.LazierUser;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
     Optional<Weather> findTopByLazierUserOrderByUpdatedAtDesc (LazierUser lazierUser);
+    List<Weather> findAllByUpdatedAtBefore(String date);
 }
