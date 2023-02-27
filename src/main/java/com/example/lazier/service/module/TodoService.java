861a3e7c23369a7751c090d015b709cab7e52377
oooo91 <74234719+oooo91@users.noreply.github.com>
2023-02-28 03:22:58 +0900

    Update TodoService.java

--- src/main/java/com/example/lazier/service/module/TodoService.java
+++ src/main/java/com/example/lazier/service/module/TodoService.java
@@ -30,7 +30,7 @@ public class TodoService {
 		Long userId = memberService.parseUserId(request);
 		LazierUser lazierUser = memberService.searchMember(userId);
 
-		if (todoRepository.countByUserId(userId) >= 3) {
+		if (todoRepository.countByUserId(userId) > 3) {
 			throw new FailedWriteException("할 일은 3개까지 작성할 수 있습니다.");
 		}
 		Todo todo = todoRepository.save(Todo.of(lazierUser, todoWriteRequestDto));
